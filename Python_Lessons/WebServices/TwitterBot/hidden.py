4ef5fac7086c5253e595cd8a8bad547794885613
SamDunny <samdunny.edu11@gmail.com>
2021-06-29 15:18:50 -0400

    trying to fix hidden exclusions

--- Python_Lessons/WebServices/TwitterBot/hidden.py
+++ Python_Lessons/WebServices/TwitterBot/hidden.py
@@ -4,7 +4,7 @@
 # Create new App and get the four strings
 
 def oauth():
-    return {"consumer_key": "uT9JmSPsOROFbQnT9qbsUiwhd",
-            "consumer_secret": "0kWjXxfgyQqmMPTar025SzKTJlNyRGdAHjQnZaubjOIfB2lT2g",
-            "token_key": "1339978359091159040-CCnJgzOFZVRf9iTFrsVBNMIV1dk0br",
-            "token_secret": "JOSkQAbVuFkRCctdYeg7kB2g7OPBfqPPKqH7nkqSx8vKu"}
+    return {"consumer_key": "testkey",
+            "consumer_secret": "testkey",
+            "token_key": "testkey",
+            "token_secret": "testkey"}
