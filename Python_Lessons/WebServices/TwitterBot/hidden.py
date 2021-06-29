aedf28b13bcd1b546336344090b91f2310cc161a
SamDunny <samdunny.edu11@gmail.com>
2021-06-29 14:54:19 -0400

    regened codes

--- Python_Lessons/WebServices/TwitterBot/hidden.py
+++ Python_Lessons/WebServices/TwitterBot/hidden.py
@@ -4,7 +4,7 @@
 # Create new App and get the four strings
 
 def oauth():
-    return {"consumer_key": "8rCuOS4DFTCmM6Z2JVUAtXZq7",
-            "consumer_secret": "hcwXyc0uIQ5I2ot53L3JyLmvhRUT8VqDWaEVfCABYk2FZzbctQ",
-            "token_key": "1339978359091159040-3wCyLaSQ2qCReUXn8WpqwRdR70hM4U",
-            "token_secret": "xenvM2SeWTlrNq9YqPo4EujraBkStWKUBFVHn2caIB4BC"}
+    return {"consumer_key": "uT9JmSPsOROFbQnT9qbsUiwhd",
+            "consumer_secret": "0kWjXxfgyQqmMPTar025SzKTJlNyRGdAHjQnZaubjOIfB2lT2g",
+            "token_key": "1339978359091159040-CCnJgzOFZVRf9iTFrsVBNMIV1dk0br",
+            "token_secret": "JOSkQAbVuFkRCctdYeg7kB2g7OPBfqPPKqH7nkqSx8vKu"}
